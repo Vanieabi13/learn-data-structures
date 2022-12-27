5e3e58261d0c875e991b89844a1781397de498ee
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2022-12-27 13:47:22 +0530

    Practice assignment to parse input

---
+++ src/main/java/basics/ParseInputPracticeAssignment.java
@@ -0,0 +1,99 @@
+package basics;
+
+public class ParseInputPracticeAssignment {
+
+
+    static int takeIntegerInput(){
+        int a = -1;
+
+        /**
+         * TODO 1: Declare Scanner object
+         */
+
+        /**
+         * TODO 2: Store solution to variable a
+         */
+
+        System.out.println("Received Input "+a);
+        return a;
+    }
+
+    static float takeFloatInput(){
+        float a = -1;
+
+        /**
+         * TODO 1: Declare Scanner object
+         */
+
+        /**
+         * TODO 2: Store solution to variable a
+         */
+
+        System.out.println("Received Input "+a);
+        return a;
+    }
+
+    static double takeDoubleInput(){
+        double a = -1;
+
+        /**
+         * TODO 1: Declare Scanner object
+         */
+
+        /**
+         * TODO 2: Store solution to variable a
+         */
+
+
+        System.out.println("Received Input "+a);
+        return a;
+    }
+
+    static boolean takeBooleanInput(){
+        boolean a = false;
+
+        /**
+         * TODO 1: Declare Scanner object
+         */
+
+        /**
+         * TODO 2: Store solution to variable a
+         */
+
+
+        System.out.println("Received Input "+a);
+        return a;
+    }
+
+    static String takeStringInput() {
+        String a = "";
+
+        /**
+         * TODO 1: Declare Scanner object
+         */
+
+        /**
+         * TODO 2: Store solution to variable a
+         */
+
+        System.out.println("Received Input "+a);
+        return a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Give integer input:");
+        takeIntegerInput();
+
+        System.out.println("Give float input:");
+        takeFloatInput();
+
+        System.out.println("Give boolean input:");
+        takeBooleanInput();
+
+        System.out.println("Give double input:");
+        takeDoubleInput();
+
+        System.out.println("Give string input:");
+        takeStringInput();
+    }
+}
