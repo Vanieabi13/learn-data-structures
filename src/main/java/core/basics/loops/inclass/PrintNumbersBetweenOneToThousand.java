6b21120de2ad309031a7bcdaa645d0837081d97e
Hardik Khandelwal <khandelwal.hardik14@gmail.com>
2023-01-02 21:33:47 +0530

    Inclass assignment on print 1 to 1000

---
+++ src/main/java/core/basics/loops/inclass/PrintNumbersBetweenOneToThousand.java
@@ -0,0 +1,14 @@
+package core.basics.loops.inclass;
+
+public class PrintNumbersBetweenOneToThousand {
+    public static void main(String[] args) {
+        /**
+         * Task is to print numbers between 1 to 1000.
+         */
+
+        // Todo : Add initialization code
+
+        // Todo: Add loop with condition and update logic
+
+    }
+}
